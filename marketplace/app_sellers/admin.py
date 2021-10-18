d9ade3979479b9dfbf85f3f73bf12ce89ba86102
Сунцов Владимир <vovka.steel911@mail.ru>
2021-10-18 09:04:46 +0300

    Admin panel and settings

--- marketplace/app_sellers/admin.py
+++ marketplace/app_sellers/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_sellers.models import (
+    Balances,
+    Goods,
+    GoodsDescriptionsValues,
+    Sellers,
+)
+
+
+@admin.register(Sellers)
+class SellersAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(GoodsDescriptionsValues)
+class GoodsDescriptionsValuesAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Goods)
+class GoodsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Balances)
+class BalancesAdmin(admin.ModelAdmin):
+    pass
+
+
