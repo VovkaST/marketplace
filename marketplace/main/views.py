72da115dd6cd8864c5e70f068d46e3e82bb37293
Mikhail <rymv@yandex.ru>
2021-10-21 21:38:53 +0300

    banners TODO 3 mixin

--- marketplace/main/views.py
+++ marketplace/main/views.py
@@ -1,20 +1,26 @@
 import random
 
-from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic.base import ContextMixin, TemplateView
 
 from .models import Banner
 
 
-class MarketMain(View):
-    """Представление главной страницы магазина"""
+class BannerMixin(ContextMixin):
+    """Выбор баннеров и занесение их в context"""
 
-    def get(self, request):
-        # формирование баннера
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
         all_active_banners = Banner.objects.filter(activity=True)
         if all_active_banners.count() > 3:
             banners = random.sample(all_active_banners, 3)
         else:
             banners = all_active_banners
+        context["banners"] = banners
+        return context
 
-        return render(request, "main/index.html", context={"banners": banners})
+
+class MarketMain(TemplateView, BannerMixin):
+    """Представление главной страницы магазина"""
+
+    template_name = "main/index.html"
