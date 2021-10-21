a0ead5c191b12730bdf5419397c509d0ded00ebe
Сунцов Владимир <vovka.steel911@mail.ru>
2021-10-21 22:07:21 +0300

    Slug-field added to Sellers model

--- marketplace/app_sellers/models.py
+++ marketplace/app_sellers/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 from django.utils.translation import gettext as _
 
+from services.models import slugify
+
 
 class Sellers(models.Model):
+    slug = models.SlugField(blank=True)
     name = models.CharField(_('Seller name'), max_length=254)
     address = models.CharField(_('Address'), max_length=254)
     email = models.EmailField(_('E-mail address'), max_length=255)
     phone = models.CharField(_('Phone number'), max_length=16)
     image = models.ImageField(_('Image (avatar)'), upload_to='files/sellers/', blank=True)
     description = models.CharField(_('Description'), max_length=1000)
+
+    def clean(self):
+        self.slug = slugify(self.name)
 
     class Meta:
         db_table = 'mp_sellers'
