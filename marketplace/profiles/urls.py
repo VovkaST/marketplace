34796bb4329d8c31a233dd6b7eb94d1998771b2c
Ковалев Глеб <kogleser@gmail.com>
2021-11-19 17:10:07 +0000

    Resolve "28 Разработка страницы «История заказов»"

--- marketplace/profiles/urls.py
+++ marketplace/profiles/urls.py
@@ -1,16 +1,17 @@
-from django.urls import path, include
-from profiles.views import (
-    AccountView,
-    ClientLoginView,
-    RegistrationView,
-    UpdateProfile,
-)
+from django.urls import include, path
 
+# fmt: off
+from profiles.views import (AccountView, ClientLoginView, RegistrationView,  # isort:skip
+                            UpdateProfile, ViewsHistoryView, OrdersHistoryView)  # isort:skip
+
+# fmt: on
 
 urlpatterns = [
-    path('account/', AccountView.as_view(), name='account'),
-    path('profile/', UpdateProfile.as_view(), name='profile'),
-    path('registration/', RegistrationView.as_view(), name='registration'),
-    path('login/', ClientLoginView.as_view(), name='login'),
-    path('', include('django.contrib.auth.urls')),
+    path("account/", AccountView.as_view(), name="account"),
+    path("profile/", UpdateProfile.as_view(), name="profile"),
+    path("registration/", RegistrationView.as_view(), name="registration"),
+    path("login/", ClientLoginView.as_view(), name="login"),
+    path("history-view/", ViewsHistoryView.as_view(), name="historyview"),
+    path("orders-history/", OrdersHistoryView.as_view(), name="ordershistory"),
+    path("", include("django.contrib.auth.urls")),
 ]
