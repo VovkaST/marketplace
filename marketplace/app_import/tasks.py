fcd68d6a747bd0411f3994011bda2c59ac913b31
Сунцов Владимир <vovka.steel911@mail.ru>
2021-12-02 23:45:50 +0300

    Task import_file is simplified, make return a dict answer

--- marketplace/app_import/tasks.py
+++ marketplace/app_import/tasks.py
@@ -1,42 +1,51 @@
-from importlib import import_module
+from django.apps import apps
+from django.db import transaction
 
 from app_import.models import ImportProtocol
 from marketplace import celery_app
 
 
 APPS_MAP = {
-    'sellers': 'app_sellers',
-    'goods': 'app_sellers',
+    'sellers': ('app_sellers', 'Sellers'),
+    'goods': ('app_sellers', 'Goods'),
 }
 
 
 @celery_app.task
-def import_file(protocol_id: int, model_name: str, update: bool):
+def import_file(protocol_id: int, model_name: str, update: bool) -> dict:
+    total, created, updated = 0, 0, 0
+    success, error_msg = True, None
     protocol = ImportProtocol.objects.get(id=protocol_id)
-    apps = {
-        'sellers': ('app_sellers.models', 'Sellers'),
-        'goods': ('app_sellers.models', 'Goods'),
-    }
-    package, class_name = apps[model_name]
-    try:
-        imported = import_module(package)
-    except ModuleNotFoundError:
-        # todo: Ошибка импорта
-        return
+    app, class_name = APPS_MAP[model_name]
 
-    model = getattr(imported, class_name)
+    model = apps.get_model(app_label=app, model_name=class_name)
     with open(file=protocol.filename.path, encoding='utf-8', mode='r') as datafile:
         headers = next(datafile).split(';')
-        for row in datafile:
-            obj = model()
-            data = dict(zip(headers, row.split(';')))
-            obj.set_values(data=data)
+        with transaction.atomic():
+            for row in datafile:
+                obj = model()
+                data = dict(zip(headers, row.split(';')))
+                obj.set_values(data=data)
 
-            n_key = obj.natural_key()
-            obj_in_db = model.objects.get_by_natural_key(**n_key)
-            if obj_in_db and update:
-                obj = obj_in_db.set_values(data=data)
-            if not obj_in_db or (obj_in_db and update):
-                obj.save()
-
-    return True
+                n_key = obj.natural_key()
+                obj_in_db = model.objects.get_by_natural_key(**n_key)
+                if obj_in_db and update:
+                    obj = obj_in_db.set_values(data=data)
+                    updated += 1
+                if not obj_in_db or (obj_in_db and update):
+                    try:
+                        obj.save()
+                    except Exception as exc:
+                        success = False
+                        error_msg = exc.args[0]
+                    created += 1
+                total += 1
+    return {
+        'success': success,
+        'error': {
+            'message': error_msg,
+        },
+        'total': total,
+        'created': created,
+        'updated': updated,
+    }
