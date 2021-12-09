d6fbc10f38eabf833aa96116ac4eb6e07e3f0165
Сунцов Владимир <vovka.steel911@mail.ru>
2021-12-09 22:10:26 +0300

    NaturalKeyModel documentation

--- marketplace/services/models.py
+++ marketplace/services/models.py
@@ -1,8 +1,18 @@
 class NaturalKeyModel:
-    def set_values(self, data):
+    """Модель, добавляющая функционал получения натурального ключа объекта."""
+
+    def set_values(self, data: dict):
+        """Установка значений атрибутов экземпляра из словаря data, где
+        ключи - это его атрибуты.
+
+        :param data: Словарь значений для установки.
+        :return: Текущий экземпляр объекта.
+        """
         for field_name, value in data.items():
             setattr(self, field_name, value)
         return self
 
     def natural_key(self):
+        """Базовый метод получения натурального ключа модели. Переопределяется
+        в моделях-наследниках."""
         raise NotImplementedError('Method is not implemented')
